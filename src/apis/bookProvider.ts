38d0dd9ea1a60180529b4283f78ac553b42af6e9
SirKaelan <kaelanfreakerman6@gmail.com>
2024-01-11 15:21:00 +0200

    Slight cleanup of BookProvider class

--- src/apis/bookProvider.ts
+++ src/apis/bookProvider.ts
@@ -5,18 +5,10 @@ import { BookResponse, PaginatedBooksResponse } from "apis/types";
 import { createArrayChunks } from "utils";
 
 class BookProvider {
+  // Might be used in the future
   private url: string = "";
 
   // Methods
-  async getAll(): Promise<Book[]> {
-    // Fetching logic goes here
-    // Make a delay here
-    const editedBooksData: Book[] = booksData.map((x): Book => {
-      return { ...x, type: "book" };
-    });
-    return editedBooksData;
-  }
-
   async getById(id: number): Promise<BookResponse> {
     const fetchedBook = booksData.find((x) => x.id === id);
     if (!fetchedBook) {
@@ -30,7 +22,6 @@ class BookProvider {
   }
 
   // Temporary method
-  // TODO: Fix edge case (page number is too high)
   private findDataChunk(chunkedData: APIBookFormat[][], page: number): Book[] {
     return chunkedData[page - 1]?.map((x): Book => {
       return {
