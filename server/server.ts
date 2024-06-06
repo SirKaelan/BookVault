5a3e068e555edd6d77d24f52b9a593ce903b138b
SirKaelan <kaelanfreakerman6@gmail.com>
2024-06-06 12:27:59 +0300

    Fix route matching logic

--- server/server.ts
+++ server/server.ts
@@ -9,13 +9,26 @@ const db = new Database();
 const authors = new Author(db);
 
 app.use(async (ctx) => {
-  if (ctx.url === "/") {
+  const url = ctx.URL.pathname;
+  let params: ExtractedParams | null;
+
+  if (url === "/") {
     ctx.response.status = 200;
     ctx.response.body = "Hello!";
+    return;
+  }
 
-    // Get author by id
-  } else if (ctx.url.startsWith("/authors/")) {
-    const authorId = ctx.url.split("/")[2];
+  // Author routes
+  params = matchRoute(url, "/authors/:id/books");
+  if (params) {
+    ctx.response.status = 200;
+    ctx.response.body = `Attempting to request books of author with "id: ${params.id}"`;
+    return;
+  }
+
+  params = matchRoute(url, "/authors/:id");
+  if (params) {
+    const authorId = params.id;
     const author = await authors.getAuthorById(authorId);
     if (author) {
       ctx.response.status = 200;
@@ -24,13 +37,49 @@ app.use(async (ctx) => {
       ctx.response.status = 404;
       ctx.response.body = `Author with ID: ${authorId} doesn't exist.`;
     }
-
-    // Entered endpoint isn't supported
-  } else {
-    ctx.response.message = "Endpoint not supported.";
+    return;
   }
+
+  // Entered endpoint isn't supported
+  ctx.status = 404;
+  ctx.response.message = "Page not found.";
 });
 
 console.log(`Server is listening on port ${PORT}.`);
 
 app.listen(PORT);
+
+type ExtractedParams = { [key: string]: string };
+
+const matchRoute = (
+  originalPath: string,
+  customPath: string
+): ExtractedParams | null => {
+  // Get endpoint segments and remove empty string
+  const originalSegments = originalPath
+    .split("/")
+    .filter((segment) => segment.length > 0);
+  const customSegments = customPath
+    .split("/")
+    .filter((segment) => segment.length > 0);
+
+  // Number of segments must match
+  if (originalSegments.length !== customSegments.length) return null;
+
+  // Contains a key-value pair for dynamic segment (:id => id: 2)
+  const params: ExtractedParams = {};
+
+  // Logic to extract dynamic values like ":id"
+  for (let i = 0; i < customSegments.length; i++) {
+    if (customSegments[i].startsWith(":")) {
+      const paramName = customSegments[i].slice(1);
+      params[paramName] = originalSegments[i];
+
+      // If number of segments match, but their names don't
+    } else if (originalSegments[i] !== customSegments[i]) {
+      return null;
+    }
+  }
+
+  return params;
+};
