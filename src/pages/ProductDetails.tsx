e50d335c550b7e1d0277e7472387555339171b73
SirKaelan <kaelanfreakerman6@gmail.com>
2024-07-29 16:37:02 +0300

    Begin work on Product Details page

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,5 +1,12 @@
 import { useGetQueryValue } from "utils";
 import { useFetchBook } from "hooks";
+import { Box, Grid, Typography, styled, Theme } from "@mui/material";
+
+const PriceTag = styled(Typography)(({ theme }: PriceTagProps) => ({
+  borderBottom: `2px solid ${theme.palette.secondary.main}`,
+}));
+
+const SynopsisContent = styled(Typography)({});
 
 export const ProductDetails = (): JSX.Element => {
   const bookId = useGetQueryValue("id");
@@ -14,8 +21,96 @@ export const ProductDetails = (): JSX.Element => {
   }
 
   return (
-    <div>
-      Book title: {book.title}, Author: {book.author_name}
-    </div>
+    <Box>
+      <Grid container columnSpacing={4}>
+        <Grid item xs={4}>
+          Image column
+        </Grid>
+        <Grid item xs={8}>
+          {/* Book title + Author */}
+          <Box>
+            <Typography
+              variant="h2"
+              component="h2"
+              fontWeight="fontWeightMedium"
+              mb={0.5}
+            >
+              {book.title}
+            </Typography>
+            <Typography
+              variant="h4"
+              component="h4"
+              fontWeight="fontWeightLight"
+            >
+              {book.author_name}
+            </Typography>
+          </Box>
+          {/* Book price */}
+          <Box my={3.5}>
+            <PriceTag
+              variant="h3"
+              component="span"
+              fontWeight="fontWeightRegular"
+            >
+              $16.99
+            </PriceTag>
+          </Box>
+          {/* Synopsis */}
+          <Box>
+            <Typography
+              variant="h4"
+              component="h4"
+              fontWeight="fontWeightMedium"
+              mb={1}
+            >
+              Synopsis:
+            </Typography>
+            {/* TODO: Need to add specific synopses to books in database and use synopsis value from object */}
+            <SynopsisContent
+              variant="h5"
+              component="p"
+              fontWeight="fontWeightLight"
+            >
+              Roshar is a world of stone and storms. Uncanny tempests of
+              incredible power sweep across the rocky terrain so frequently that
+              they have shaped ecology and civilization alike. Animals hide in
+              shells, trees pull in branches, and grass retracts into the
+              soilless ground. Cities are built only where the topography offers
+              shelter.It has been centuries since the fall of the ten
+              consecrated orders known as the Knights Radiant, but their
+              Shardblades and Shardplate remain: mystical swords and suits of
+              armor that transform ordinary men into near-invincible warriors.
+              Men trade kingdoms for Shardblades. Wars were fought for them, and
+              won by them.One such war rages on a ruined landscape called the
+              Shattered Plains. There, Kaladin, who traded his medical
+              apprenticeship for a spear to protect his little brother, has been
+              reduced to slavery. In a war that makes no sense, where ten armies
+              fight separately against a single foe, he struggles to save his
+              men and to fathom the leaders who consider them
+              expendable.Brightlord Dalinar Kholin commands one of those other
+              armies. Like his brother, the late king, he is fascinated by an
+              ancient text called The Way of Kings. Troubled by over-powering
+              visions of ancient times and the Knights Radiant, he has begun to
+              doubt his own sanity.Across the ocean, an untried young woman
+              named Shallan seeks to train under an eminent scholar and
+              notorious heretic, Dalinar's niece, Jasnah. Though she genuinely
+              loves learning, Shallan's motives are less than pure. As she plans
+              a daring theft, her research for Jasnah hints at secrets of the
+              Knights Radiant and the true cause of the war.The result of over
+              ten years of planning, writing, and world-building, The Way of
+              Kings is but the opening movement of the Stormlight Archive, a
+              bold masterpiece in the making.Speak again the ancient oaths:Life
+              before death.Strength before weakness.Journey before
+              Destination.and return to men the Shards they once bore.The
+              Knights Radiant must stand again.
+            </SynopsisContent>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
+};
+
+type PriceTagProps = {
+  theme: Theme;
 };
