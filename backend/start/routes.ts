7ec9d8baeb5d87928d0cf05a3f3b9277d92c332d
SirKaelan <kaelanfreakerman6@gmail.com>
2025-10-31 10:58:46 +0200

    Add previously available route endpoints to Adonis

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -10,7 +10,27 @@
 import router from '@adonisjs/core/services/router'
 
 router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
+  return 'Home endpoint!'
+})
+
+// Book endpoints
+router.get('/books', async () => {
+  return 'Paginated books endpoint!'
+})
+
+router.get('/books/:id', async ({ params }) => {
+  return `Single book data with id of ${params.id} endpoint!`
+})
+
+router.get('/books/:id/genres', async ({ params }) => {
+  return `All genres of a single book with id of ${params.id} endpoint!`
+})
+
+// Author endpoints
+router.get('/authors/:id', async ({ params }) => {
+  return `Single author data with id of ${params.id} endpoint!`
+})
+
+router.get('/authors/:id/books', async ({ params }) => {
+  return `All books of a single author with id of ${params.id} endpoint!`
 })
