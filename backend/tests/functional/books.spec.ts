c0a78fc0426264cd6e13d6a19a6bbe0c754c3624
sirkaelan <kaelanfreakerman6@gmail.com>
2025-11-26 17:30:04 +0200

    Add tests for requesting books with query params

--- backend/tests/functional/books.spec.ts
+++ backend/tests/functional/books.spec.ts
@@ -1,14 +1,13 @@
 import { test } from '@japa/runner'
 
 import Book from '#models/book'
-import { PaginatedResponse } from '#types/index'
+import { ErrorResponse, PaginatedResponse } from '#types/index'
 import testUtils from '@adonisjs/core/services/test_utils'
 import { BookFactory } from '#database/factories/book_factory'
 
 test.group('Books - list (GET /books)', (group) => {
   group.each.setup(() => testUtils.db().withGlobalTransaction())
 
-  // test happy path
   test('returns 200 and paginated list of books when there are books', async ({
     client,
     expect,
@@ -33,7 +32,33 @@ test.group('Books - list (GET /books)', (group) => {
     expect(data).toBeInstanceOf(Array)
     expect(data.length).toBe(0)
   })
-  // test unhappy 422 response (bad QPs)
+
+  test('returns 200 and paginated book when requesting page_size of 1', async ({
+    client,
+    expect,
+  }) => {
+    await BookFactory.createMany(3)
+
+    const response = await client.get('/books').qs({ page: 1, page_size: 1 })
+
+    response.assertStatus(200)
+
+    let { data } = response.body() as PaginatedResponse<Book>
+    expect(data.length).toBe(1)
+  })
+
+  test('returns 422 and error objects when bad query params are given', async ({
+    client,
+    expect,
+  }) => {
+    const response = await client.get('/books').qs({ page: 'wrong value', page_size: 0.25 })
+
+    response.assertStatus(422)
+
+    let { errors } = response.body() as ErrorResponse
+    expect(errors).toBeInstanceOf(Array)
+    expect(errors.length).toBe(2)
+  })
 })
 
 test.group('Books - get one (GET /books/:id)', () => {
