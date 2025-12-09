9f06720c5ee9628d728ca6de3cbc2053ba783b71
SirKaelan <kaelanfreakerman6@gmail.com>
2025-12-09 14:01:13 +0200

    Try out chakra ui by making a page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,50 @@
 import React from "react";
 
+import { HStack, Stack } from "@chakra-ui/react/stack";
+import { AspectRatio } from "@chakra-ui/react/aspect-ratio";
+import { Image } from "@chakra-ui/react/image";
+import { Heading } from "@chakra-ui/react/heading";
+import { Center } from "@chakra-ui/react/center";
 import { Button } from "@chakra-ui/react/button";
-import { HStack } from "@chakra-ui/react/stack";
-import { useColorMode } from "@/components/ui/color-mode";
+import { Box } from "@chakra-ui/react/box";
 
 export const Home = (): React.JSX.Element => {
-  const { toggleColorMode, colorMode } = useColorMode();
-
   return (
     <>
-      <div>Home page!</div>
-      <div>
-        Color mode is: <b>{colorMode.toUpperCase()}</b>
-      </div>
-      <HStack>
-        <Button variant="outline" onClick={toggleColorMode}>
-          Button
-        </Button>
-        <Button variant="outline">Another button!</Button>
-      </HStack>
+      <Stack gap="20">
+        <Heading size="6xl">
+          <Center>Welcome to Book Vault</Center>
+        </Heading>
+
+        {/* Featured books */}
+        <Stack gap="5">
+          <Heading size="4xl">Featured books</Heading>
+          <Box position="relative">
+            <HStack gap="5" overflow="hidden">
+              {[...Array(8)].map(() => (
+                <AspectRatio minW="150px" ratio={1 / 1.6}>
+                  <Image
+                    src="https://m.media-amazon.com/images/I/81pJXhRLdoL._AC_UF1000,1000_QL80_.jpg"
+                    alt="The Way of Kings book cover"
+                  />
+                </AspectRatio>
+              ))}
+            </HStack>
+            <Button
+              position="absolute"
+              right="-10px"
+              top="50%"
+              transform="translateY(-50%)"
+              size="xl"
+              colorPalette="teal"
+              variant="subtle"
+              rounded="full"
+            >
+              {">"}
+            </Button>
+          </Box>
+        </Stack>
+      </Stack>
     </>
   );
 };
