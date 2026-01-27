2b81e2ba0e5852bff6b4579309646f8cbf0747f7
sirkaelan <kaelanfreakerman6@gmail.com>
2026-01-27 11:53:26 +0200

    Simplify home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,49 +1,12 @@
 import React from "react";
 
-import { HStack, Stack } from "@chakra-ui/react/stack";
-import { AspectRatio } from "@chakra-ui/react/aspect-ratio";
-import { Image } from "@chakra-ui/react/image";
 import { Heading } from "@chakra-ui/react/heading";
 import { Center } from "@chakra-ui/react/center";
-import { Button } from "@chakra-ui/react/button";
-import { Box } from "@chakra-ui/react/box";
 
 export const Home = (): React.JSX.Element => {
   return (
-    <>
-      <Stack gap="20">
-        <Heading size="4xl">
-          <Center>Welcome to Book Vault</Center>
-        </Heading>
-
-        {/* Featured books */}
-        <Stack gap="5">
-          <Heading size="2xl">Featured books</Heading>
-          <Box position="relative">
-            <HStack gap="5" overflow="hidden">
-              {[...Array(8)].map(() => (
-                <AspectRatio minW="150px" ratio={1 / 1.6}>
-                  <Image
-                    src="https://m.media-amazon.com/images/I/81pJXhRLdoL._AC_UF1000,1000_QL80_.jpg"
-                    alt="The Way of Kings book cover"
-                  />
-                </AspectRatio>
-              ))}
-            </HStack>
-            <Button
-              position="absolute"
-              right="-20px"
-              top="50%"
-              transform="translateY(-50%)"
-              colorPalette="teal"
-              variant="subtle"
-              rounded="full"
-            >
-              {">"}
-            </Button>
-          </Box>
-        </Stack>
-      </Stack>
-    </>
+    <Center>
+      <Heading size="4xl">Welcome to Book Vault</Heading>
+    </Center>
   );
 };
